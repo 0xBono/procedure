46232fe3cdfa1437225971775746485c3bc080dc
0xbono <iam@sonjim.in>
2021-08-14 07:41:26 +0900

    feat(containers/main): 캐로셀 컴포넌트 컨테이너에 임시적인 추가

--- containers/Main/Main.tsx
+++ containers/Main/Main.tsx
@@ -1,3 +1,13 @@
+import { Carousel } from 'components';
+import classNames from 'classnames/bind';
+import styles from './Main.module.scss';
+
+const cx = classNames.bind(styles);
+
 export const Main = () => {
-  return <></>;
+  return (
+    <section className={cx('wrap')}>
+      <Carousel />
+    </section>
+  );
 };
